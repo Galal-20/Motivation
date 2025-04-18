fe9dc7fc5455f44354a223cc482ad7d6da953ccc
Galal-20 <ga71387@gmail.com>
2025-04-18 16:20:31 +0200

    Initial Commit

---
+++ Motivation/MotivationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationApp.swift
+//  Motivation
+//
+//  Created by Dr.Galal Ahmed  on 18/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MotivationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MotivationTests/MotivationTests.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationTests.swift
+//  MotivationTests
+//
+//  Created by Dr.Galal Ahmed  on 18/04/2025.
+//
+
+import Testing
+@testable import Motivation
+
+struct MotivationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
